e2e6046a3f4a7717d7c13b9c1ef41d3be0c4d6ff
stanley <stanley.hsu.x@gmail.com>
2024-01-16 15:38:04 +0800

    .

--- geisha/src/register/fake.tsx
+++
@@ -1,7 +0,0 @@
-import { registerApiService, registerHttpParser } from 'common/other/register';
-import { ApiFakeService } from 'common/util/apiFake';
-
-console.log('FAKE MODEL');
-if (import.meta.env.VITE_FAKE) {
-    registerApiService(ApiFakeService);
-}
