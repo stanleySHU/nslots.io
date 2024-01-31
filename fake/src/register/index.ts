e72272d73ac7a5d99993106ae464aa947edfccd2
stanley <stanley.hsu.x@gmail.com>
2024-01-31 17:05:34 +0800

    .

--- fake/src/register/index.ts
+++
@@ -1,9 +0,0 @@
-import './components';
-import './model';
-
-import { registerApiService, registerHttpParser } from 'common/other/register';
-import { ApiService } from 'common/util/api';
-import { SpinModel } from 'common/util/parser/spin/queenBee';
-
-registerApiService(ApiService);
-registerHttpParser('spin', SpinModel);
