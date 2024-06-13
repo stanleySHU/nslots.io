b4b37b3ad48b01aa2b4656ab73af7d49ad8c1cd6
stanley <stanley.hsu.x@gmail.com>
2024-06-13 12:25:12 +0800

    .

--- goldenmahjongarena/lib/bootstrap.js
+++ goldenmahjongarena/lib/bootstrap.js
@@ -24,7 +24,7 @@ function bootstrap(config) {
     var u = document;
     var o = "script";
     var y = u.createElement(o);
-    y.src = "./js/app." + slotapp.config["js"] + ".js?v=1718186783861";
+    y.src = "./js/app." + slotapp.config["js"] + ".js?v=1718252603048";
     y.onreadystatechange = function () {
         if (this.readyState == 'complete' || this.readyState == 'loaded') {
             startGame();
