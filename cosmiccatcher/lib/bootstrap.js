8b67a98ad146946e89fe1807f0ced4d05fee29ae
stanley <stanley.hsu.x@gmail.com>
2024-08-08 00:48:55 +0800

    .

--- cosmiccatcher/lib/bootstrap.js
+++ cosmiccatcher/lib/bootstrap.js
@@ -24,7 +24,7 @@ function bootstrap(config) {
     var u = document;
     var o = "script";
     var y = u.createElement(o);
-    y.src = "./js/app." + slotapp.config["js"] + ".js?v=1723049066446";
+    y.src = "./js/app." + slotapp.config["js"] + ".js?v=1723049277881";
     y.onreadystatechange = function () {
         if (this.readyState == 'complete' || this.readyState == 'loaded') {
             startGame();
